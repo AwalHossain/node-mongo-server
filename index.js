f39cea4d6d9f3877678b34c5b979e12a352c2b96
AwalHossain <waqin21@gmail.com>
2021-10-23 22:11:49 +0600

    update section added

--- index.js
+++ index.js
@@ -1,78 +1,86 @@
-const express = require ('express');
-const cors = require('cors')
-const { MongoClient } = require('mongodb');
+const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors')
 app.use(cors())
-const ObjectId = require('mongodb').ObjectId;
-
+const port = 5000
 
 app.use(express.json())
-app.get('/', ( req,res)=>{
-    res.send('Crud server')
+// import { MongoClient } from "mongodb";
+const {MongoClient} = require ('mongodb')
+const ObjectId = require('mongodb').ObjectId
+app.get('/', (req, res)=>{
+  res.send("Hello world")
+  console.log("hello")
 })
 
-// name: mydb
-//pasword : MxvAqcyeIkpVB8QW
-
-
-
-
-
-// Replace the uri string with your MongoDB deployment's connection string.
 const uri = "mongodb+srv://mydb:MxvAqcyeIkpVB8QW@cluster0.33slg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri);
-
-
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run(){
   try{
     await client.connect();
-    const database = client.db('newDb');
-    const userCollection = database.collection('users');
-
-    app.get("/users", async (req,res)=>{
-      const cursor = userCollection.find({})
-      const users = await cursor.toArray();
-
-      res.send(users)
+    const database = client.db("ProdutList")
+    const dbCollection = database.collection('products')
+    // const doc ={
+    //   name:"watch",
+    //   price:89
+    // }
+    // const result = await dbCollection.insertOne(doc)
+    // console.log("hitting the dirt", result);
+    app.get('/products', async(req, res)=>{
+      const cursor = dbCollection.find({})
+      const products = await cursor.toArray();
+      res.send(products)
     })
-
-
-    app.post('/users', async (req, res)=>{
+    app.post('/products', async (req, res)=>{
       const newUser = req.body;
-    const result = await  userCollection.insertOne(newUser)
-      console.log("hittting the psot",req.body);
+      const result = await dbCollection.insertOne(newUser)
+      console.log("hitting the post", req.body);
+      // res.send("post working")
       res.json(result)
       console.log(result);
     })
 
-    app.get('/users/:id', async(req, res)=>{
+    app.get('/products/:id', async (req, res)=>{
       const id = req.params.id;
+      const query = {_id:ObjectId(id)}
+      const products = await dbCollection.findOne(query)
+      console.log("dynamic id",id)
+      res.send(products)
+    })
+    app.put('/products/:id', async(req, res)=>{
+      const id = req.params.id;
+      const updateUser = req.body;
+   
       const query = {_id: ObjectId(id)}
-      const user = await userCollection.findOne(query)
-      console.log("load user with", id);
-      res.send(user)
+      const option = {upsert:true}
+      const updateDoc = {
+        $set:{
+          name:updateUser.name,
+          price:updateUser.price,
+          quantity:updateUser.quantity
+        }
+      }
+      const result = await dbCollection.updateOne(query, updateDoc, option)
+      console.log("updatin user", result);
+      res.send(result)
     })
-
-    app.delete('/users/:id', async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id: ObjectId(id)};
-      const result = await userCollection.deleteOne(query)
-      console.log('deleting the id',result, id)
-      res.json(1)
+    app.delete('/products/:id', async (req, res)=>{
+      const id= req.params.id;
+      const query ={_id:ObjectId(id)}
+      const products = await dbCollection.deleteOne(query)
+      console.log("deleting the id", products);
+      res.json(products)
     })
   }
-
   finally{
-    // await client.close()
+  //  await client.close()
   }
-
 }
 
 run().catch(console.dir)
 
 app.listen(port, ()=>{
-    console.log('Running server on', port);
+  console.log("App listring on", port);
 })
